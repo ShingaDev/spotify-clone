6c2a08894929cc614c6b2fc83beda5e91a1e28b3
Rageex <emre.artdesign@gmail.com>
2022-03-31 15:18:12 +0300

    Install and customize
    Install tailwind.css and customize.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  content: [],
+  mode: 'jit',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backdrop: '#121212',
+        primary: '#1db954',
+        active: '#3282828',
+        link: '#b3b3b3',
+        footer: '#181818'
+      }
+    },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 }
